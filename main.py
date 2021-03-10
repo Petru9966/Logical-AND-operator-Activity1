ad7a3a1d399e68d4334574f39e1a5cc75306b65b
repl.it user <replituser@example.com>
2021-03-10 19:46:26 +0000

    Activity 1 with AND logical operator

---
+++ main.py
@@ -0,0 +1,11 @@
+#Beep is asking what is he hearing
+print("What did I hear?")
+hear=input()
+#Beep is asking what is he seeing
+print("What did I see?")
+see=input()
+#Determine the tipe of reaction
+if((hear=="grr") and (see=="two red eyes")):
+  print("There is a scary crature. I should get out of here!")
+else:
+  print("I am a litlle bit scared but I will continue.")
